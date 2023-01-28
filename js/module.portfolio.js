34a2377a6844524cec9af3b05483a0f79406fe09
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2023-01-27 22:13:34 -0600

    added humanizer library for time since last commit

--- js/module.portfolio.js
+++ js/module.portfolio.js
@@ -24,7 +24,7 @@ let jkPortfolio = {
                     <div class="card-body">
                         <h6 class="card-subtitle text-muted mb-2">
                             <i class="bi bi-star-fill"></i> Stars: ${repo.stargazers_count}
-                            <i class="bi bi-calendar3"></i> Updated: ${repo.updated_at}
+                            <i class="bi bi-calendar3"></i> Last Commit: ${this.getUpdatedTimeElapsed(repo.pushed_at)} ago
                         </h6>
                         <p class="card-text">${repo.description}</p>
                         <a href="${repo.html_url}" class="card-link" target="_blank">View on GitHub</a>
@@ -42,6 +42,12 @@ let jkPortfolio = {
             html += `<span class="badge bg-light">${topic}</span> `;
         }
         return html;
+    },
+    getUpdatedTimeElapsed: function(updated) {
+        var updatedDate = new Date(updated);
+        var nowDate = new Date();
+        var diff = nowDate.getTime() - updatedDate.getTime();
+        return humanizeDuration(diff, { largest: 2, units: [ 'y', 'mo', 'd', 'h', 'm', 's' ] });
     }
 
 };
