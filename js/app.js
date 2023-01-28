fc1c6ff3a4f106d63232eba11e78c074aaf81164
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2023-01-27 22:17:59 -0600

    portfolio github integration (#31)
    * updated javascript to be module based
    * added pull portfolio from github repos
    * added humanizer library for time since last commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+import jkBlog from "./module.blog.js";
+import jkPortfolio from "./module.portfolio.js";
+import jkTagList from "./module.tag-list.js";
+
+window.jk = {
+    blog: jkBlog,
+    portfolio: jkPortfolio,
+    tagList: jkTagList
+};
---
+++ js/module.portfolio.js
@@ -0,0 +1,55 @@
+let jkPortfolio = {
+    root: "https://api.github.com",
+    includedRepositoryNames: [
+        "JK.Common",
+        "photo-sync",
+        "photocollage",
+        "presentations",
+        "scripts",
+        "tip-calc"
+    ], 
+    init: async function() {
+        let response = await fetch(`${this.root}/users/jeremyknight-me/repos`);
+        let repos = await response.json();
+        let app = document.querySelector("#github-repos");
+        let html = "";
+        for (let repo of repos) {
+            if (repo.archived || !this.includedRepositoryNames.includes(repo.name)) {
+                continue;
+            }
+
+            html += `
+                <div class="card mt-3">
+                    <div class="card-header">${repo.name}</div>
+                    <div class="card-body">
+                        <h6 class="card-subtitle text-muted mb-2">
+                            <i class="bi bi-star-fill"></i> Stars: ${repo.stargazers_count}
+                            <i class="bi bi-calendar3"></i> Last Commit: ${this.getUpdatedTimeElapsed(repo.pushed_at)} ago
+                        </h6>
+                        <p class="card-text">${repo.description}</p>
+                        <a href="${repo.html_url}" class="card-link" target="_blank">View on GitHub</a>
+                    </div>
+                    <div class="card-footer">${this.getTopicHtml(repo.topics)}</div>
+                </div>
+            `;
+        }
+
+        app.innerHTML = html;
+    },
+    getTopicHtml: function(topics) {
+        let html = "";
+        for (let topic of topics) {
+            html += `<span class="badge bg-light">${topic}</span> `;
+        }
+        return html;
+    },
+    getUpdatedTimeElapsed: function(updated) {
+        var updatedDate = new Date(updated);
+        var nowDate = new Date();
+        var diff = nowDate.getTime() - updatedDate.getTime();
+        return humanizeDuration(diff, { largest: 2, units: [ 'y', 'mo', 'd', 'h', 'm', 's' ] });
+    }
+
+};
+
+export { jkPortfolio as default };
